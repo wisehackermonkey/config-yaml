6a9abf56a1d85390eab2b5d4d806f6a461ab282c
Oran C <oranbusiness@gmail.com>
2018-12-22 10:13:33 -0800

    created dummy program, setup dynamic config
    added config.yaml to be not included in git

--- main.py
+++ main.py
@@ -5,7 +5,24 @@
 	oranbusiness@gmail.com
 	20181222
 	github.com/wisehackermonkey
-	
-	"""
+"""
+from configparser import ConfigParser
 
-	
+config = ConfigParser()
+config.read("config.yaml")
+# config.add_section("Defauts")
+# config.set("Defauts","spyagent1","password123")
+# with open("config.yaml", "w") as f:
+    # config.write(f)
+
+
+class TopSecreteProgram():
+    def __init__(self):
+        self.topsecretkey = config["Defauts"]["spyagent1"]
+        pass
+    def recon(self):
+        print(self.topsecretkey)
+
+spyagent = TopSecreteProgram()
+
+spyagent.recon()
